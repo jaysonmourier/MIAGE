c235a6b2ee3cbefcae2ce89cd428a3c8bd564e83
Jayson Mourier <mourierjayson@protonmail.com>
2022-10-11 11:27:51 +0200

    add abstract to Main.java

--- MAPC/TD2/src/Main.java
+++ MAPC/TD2/src/Main.java
@@ -1,3 +1,15 @@
+/**
+ * Jayson MOURIER - 41000228
+ *
+ * L’objet de cet exercice est de concevoir une application FileSystem permettant de créer une arborescence
+ * de fichiers, contenus dans des répertoires.
+ *
+ * Dans cette application, il existe deux types de fichiers, les fichiers ordinaires et les répertoires.
+ * - Un répertoire peut contenir des fichiers et d'autres répertoires.
+ * - Chaque fichier possède un nom (non nécessairement unique)
+ * - Chaque fichier a un propriétaire, un propriétaire a un nom unique.
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
