5fd1571e184afb09e7632bc680ef067918a48f99
Jayson Mourier <mourierjayson@protonmail.com>
2022-10-14 15:03:51 +0200

    add initialize function

--- Model/TD_TDD/src/test/java/TDTDD/DictionaryTest.java
+++ Model/TD_TDD/src/test/java/TDTDD/DictionaryTest.java
@@ -1,28 +1,34 @@
 package TDTDD;
 
 import org.example.Dictionary;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class DictionaryTest {
+    private Dictionary dict;
+    @Before
+    public void initialize()
+    {
+        dict = new Dictionary("Example");
+    }
     @Test
     public void testDictionaryName()
     {
-        assertThat((new Dictionary("Example")).getName(), equalTo("Example"));
+        assertThat(dict.getName(), equalTo("Example"));
     }
 
     @Test
     public void testDictionaryEmpty()
     {
-        assertThat((new Dictionary("testDictionaryEmpty")).isEmpty(), equalTo(true));
+        assertThat(dict.isEmpty(), equalTo(true));
     }
 
     @Test
     public void testOneTranslation()
     {
-        Dictionary dict = new Dictionary("French2English");
         dict.addTranslation("contre", "against");
         assertThat(dict.getTranslation("contre"), equalTo("against"));
     }
